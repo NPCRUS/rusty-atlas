aa33977235908956947acc81ad3da986dccd4aba
lylz <rnationtech@gmail.com>
2021-09-09 01:20:46 +0300

    atlas creation - horizontal image splicing added

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
+mod atlas_maker;
 mod flags_parser;
 
+use crate::atlas_maker::*;
+use crate::flags_parser::{parse_args, Flags};
 use std::env;
-use crate::flags_parser::{Flags, parse_args};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let parsed_args = parse_args(args);
     println!("{:?}", parsed_args);
+
+    // TODO: test usage, remove
+    // let mut file_paths = Vec::new();
+    // file_paths.push(String::from("./test_assets/RustyAtlas_R.png"));
+    // file_paths.push(String::from("./test_assets/RustyAtlas_A.png"));
+    //
+    // make_atlas(file_paths);
 }
